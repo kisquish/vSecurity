1638dc0acb33b7a63596327c89c297bba39f7081
visquis <visquis-hermann.miaffossa@capgemini.com>
2017-06-09 05:11:37 -0400

    restructuration of the playbook

--- scripts/create_role.sh
+++ scripts/create_role.sh
@@ -2,13 +2,4 @@
 
 role=$1
 
-mkdir $role
-mkdir $role/tasks
-mkdir $role/files
-mkdir $role/templates
-mkdir $role/handlers
-mkdir $role/defaults
-
-echo "---" >> $role/tasks/main.yml
-echo "" >> $role/tasks/main.yml
-echo "" >> $role/tasks/main.yml
+ansible-galaxy --offline init $role
